9c764155480c59f010eb3b8895f542481016df26
Anuja Santosh Dhanwate <dhanwateanuja@gmail.com>
2025-11-24 13:30:03 +0530

    Add files via upload

---
+++ Assignment/Assignment25/program1_2.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+void Pattern(int iRow , int iCol)
+{
+    int i , j=0;
+    if(iRow != iCol)
+        {
+            printf("Invalide input\n");
+            printf("iRow number and iCol number should be same ");
+            return;
+        }
+    for(i=1; i<=iRow ; i++)
+    {
+       
+
+        for(j=1; j<=iCol; j++)
+            {
+              if(i<=j )
+              {
+                printf("*\t");
+              }
+              
+                
+            }
+           printf("\n");
+    }
+}
+int main()
+{
+    int iValue1 = 0, iValue2=0;
+    printf("Enter number of rows : ");
+    scanf("%d",&iValue1);
+    printf("Enter number of columns : ");
+    scanf("%d",&iValue2);
+
+    Pattern(iValue1,iValue2);
+
+    return 0;
+}
